c33f304977cb3ee8044bcc63928ea9a5f03a75c5
gleam <101527589+GleammerRay@users.noreply.github.com>
2023-03-18 00:27:43 +0000

    Website structure implemented

---
+++ grayscale.js
@@ -0,0 +1,39 @@
+var documentStyle;
+
+function enableGrayscale() {
+  localStorage.setItem("grayscale", "true");
+  documentStyle = document.documentElement.style;
+  document.documentElement.style = "-moz-filter: grayscale(100%); -webkit-filter: grayscale(100%);  filter: grayscale(100%);";
+}
+
+function disableGrayscale() {
+  localStorage.setItem("grayscale", "false");
+  document.documentElement.style = documentStyle;
+  documentStyle = null;
+}
+
+function toggleGrayscale() {
+  if (documentStyle == null) {
+    enableGrayscale();
+    return;
+  }
+  disableGrayscale();
+}
+
+function loadGrayscale() {
+  grayscale = localStorage.getItem("grayscale");
+  if (grayscale == null) return;
+  if (grayscale == "false") return;
+  if (grayscale == "true") enableGrayscale();
+}
+
+loadGrayscale();
+
+window.onload = function () {
+  setTimeout(function () {
+      document.documentElement.scrollTop = 0;
+      window.scrollTo(0, 0);
+    },
+    500
+  );
+}
